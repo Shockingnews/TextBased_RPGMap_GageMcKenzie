0fee31cd02382fdb322f7edab2863d58358257d1
Shockingnews <gagemckenzie70@gmail.com>
2025-11-10 14:59:18 -0400

    done

--- TextBased_RPGMap_GageMcKenzie/Program.cs
+++ TextBased_RPGMap_GageMcKenzie/Program.cs
@@ -10,127 +10,361 @@ namespace TextBased_RPGMap_GageMcKenzie
     internal class Program
     {
         static int placeHolder = 0;
-
+        
+        static char[] border = new char[]
+        {
+            '-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-'
+        };
         static char[,] map = new char[,] // dimensions defined by following data:
     {
-        {'/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/'},
-        {'^','^','^','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
-        {'^','^','`','`','`','`','*','*','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','~','~','~','`','`','`'},
-        {'^','^','`','`','`','*','*','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','~','~','~','`','`','`','`','`'},
-        {'^','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
-        {'`','`','`','`','~','~','~','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
-        {'`','`','`','`','~','~','~','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
-        {'`','`','`','~','~','~','~','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','^','^','`','`','`','`','`','`'},
-        {'`','`','`','`','`','~','~','~','`','`','`','`','`','`','`','`','`','`','`','`','`','^','^','^','^','`','`','`','`','`'},
-        {'`','`','`','`','`','~','~','~','~','`','`','`','`','`','`','`','`','`','`','`','`','`','`','^','^','^','^','`','`','`'},
-        {'`','`','`','`','`','`','`','~','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
-        {'`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
-        {'`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
-        {'/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/','/'},
+        
+        {'^','^','^','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`',},
+        {'^','^','`','`','`','`','*','*','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','~','~','~','`','`','`',},
+        {'^','^','`','`','`','*','*','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','~','~','~','`','`','`','`','`',},
+        {'^','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`',},
+        {'`','`','`','`','~','~','~','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`',},
+        {'`','`','`','`','~','~','~','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`',},
+        {'`','`','`','~','~','~','~','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','^','^','`','`','`','`','`','`',},
+        {'`','`','`','`','`','~','~','~','`','`','`','`','`','`','`','`','`','`','`','`','`','^','^','^','^','`','`','`','`','`',},
+        {'`','`','`','`','`','~','~','~','~','`','`','`','`','`','`','`','`','`','`','`','`','`','`','^','^','^','^','`','`','`',},
+        {'`','`','`','`','`','`','`','~','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`',},
+        {'`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`',},
+        {'`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`',},
+        
     };
         static void Main(string[] args)
         {
-            Display();
+            Display(1);
+            
             
         }
 
         static void Display(int scale =1)
         {
             if (scale == 1) 
-            { 
-                for(int i = 0; i < map.GetLength(0); i++)
+            {
+                Console.Write(border);
+                placeHolder += 1;
+                Console.SetCursorPosition(0, placeHolder);
+                for (int i = 0; i < map.GetLength(0); i++)
                 {
-                
+
 
 
                     for (int j = 0; j < map.GetLength(1); j++)
                     {
-                        
+
 
                         if (j == 0)
                         {
                             Console.SetCursorPosition(0, placeHolder);
-                            Console.Write('/');
+                            
                             placeHolder += 1;
-
+                            Console.Write('|');
                         }
-                        
+                        if (map[i,j]== '`')
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write(map[i, j]);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        if (map[i, j] == '~')
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.Write(map[i, j]);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
 
-                        Console.Write(map[i,j]);
-                    
+                        if (map[i, j] == '*')
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write(map[i, j]);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        if (map[i, j] == '^')
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write(map[i, j]);
+                            
+                        }
 
 
                     }
-                    Console.Write('/');
+                    Console.Write('|');
 
                 }
+                Console.WriteLine(' ');
+                Console.Write(border);
             }
             if (scale == 2)
             {
+                Console.Write(border);
+                Console.Write(border);
+                placeHolder += 1;
+                Console.SetCursorPosition(0, placeHolder);
                 for (int i = 0; i < map.GetLength(0); i++)
                 {
-
-
-
                     for (int j = 0; j < map.GetLength(1); j++)
                     {
-                        //if (j == 29)
-                        //{
-                        //    Console.SetCursorPosition(31, placeHolder);
 
-                        //    Console.WriteLine('/');
 
-                        //}
+                        if (j == 0)
+                        {
+                            
+                            Console.SetCursorPosition(0, placeHolder);
+                            
+                            placeHolder += 1;
+                            Console.Write('|');
+                        }
+
+                        if (map[i, j] == '`')
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        if (map[i, j] == '~')
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+
+                        if (map[i, j] == '*')
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        if (map[i, j] == '^')
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+
+                        }
+
+
+
+                    }
+                    Console.Write('|');
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
+
 
                         if (j == 0)
                         {
                             Console.SetCursorPosition(0, placeHolder);
-                            Console.Write('/');
+                            Console.Write('|');
                             placeHolder += 1;
 
                         }
 
-                        Console.Write(map[i, j]);
-                        Console.Write(map[i, j]);
-                        Console.Write(map[i, j]);
+                        if (map[i, j] == '`')
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        if (map[i, j] == '~')
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+
+                        if (map[i, j] == '*')
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        if (map[i, j] == '^')
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+
+                        }
+
 
                     }
 
+                    Console.Write('|');
                 }
+                
+                Console.SetCursorPosition(0, placeHolder);
+                Console.Write(border);
+                Console.Write(border);
+                
             }
             if (scale == 3)
             {
+                Console.Write(border);
+                Console.Write(border);
+                Console.Write(border);
+                placeHolder += 1;
+                Console.SetCursorPosition(0, placeHolder);
                 for (int i = 0; i < map.GetLength(0); i++)
                 {
-
-
-
                     for (int j = 0; j < map.GetLength(1); j++)
                     {
-                        //if (j == 29)
-                        //{
-                        //    Console.SetCursorPosition(31, placeHolder);
-
-                        //    Console.WriteLine('/');
-
-                        //}
+                        
 
                         if (j == 0)
                         {
                             Console.SetCursorPosition(0, placeHolder);
-                            Console.Write('/');
+                             
                             placeHolder += 1;
+                            Console.Write('|');
+                        }
+
+                        if (map[i, j] == '`')
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        if (map[i, j] == '~')
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+
+                        if (map[i, j] == '*')
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        if (map[i, j] == '^')
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
 
                         }
 
-                        Console.Write(map[i, j]);
-                        Console.Write(map[i, j]);
-                        Console.Write(map[i, j]);
+
+                    }
+                    Console.Write('|');
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
+
+
+                        if (j == 0)
+                        {
+                            Console.SetCursorPosition(0, placeHolder);
+
+                            placeHolder += 1;
+                            Console.Write('|');
+                        }
+
+                        if (map[i, j] == '`')
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        if (map[i, j] == '~')
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+
+                        if (map[i, j] == '*')
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        if (map[i, j] == '^')
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+
+                        }
 
 
                     }
+                    Console.Write('|');
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
 
+
+                        if (j == 0)
+                        {
+                            Console.SetCursorPosition(0, placeHolder);
+
+                            placeHolder += 1;
+                            Console.Write('|');
+                        }
+
+                        if (map[i, j] == '`')
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        if (map[i, j] == '~')
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+
+                        if (map[i, j] == '*')
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        if (map[i, j] == '^')
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+                            Console.Write(map[i, j]);
+
+                        }
+
+
+                    }
+                    Console.Write('|');
                 }
+                Console.SetCursorPosition(0, placeHolder);
+                Console.Write(border);
+                Console.Write(border);
+                Console.Write(border);
             }
 
             if (scale > 3)
